f38bff36885dd2f91be4d89cc3b1cd7f9e08efc6
niannian199 <niannian199@sina.com>
2014-11-13 18:06:05 +0800

    增加查询表单状态

--- src/main/java/com/qunar/ops/oaengine/util/Constants.java
+++ src/main/java/com/qunar/ops/oaengine/util/Constants.java
@@ -3,4 +3,16 @@ package com.qunar.ops.oaengine.util;
 public interface Constants {
 	//processKey
 	public static final String DAILYSUBMIT = "日常报销";
+	
+	//finishedFlag
+	public static final int PROCESSING = 0;
+	public static final int PROC_END = 1;
+	public static final int TERMINATION = 3;
+	public static final int PROC_GRIFT = 5;
+	
+	
+	//return result
+	public static final int SUCCESS = 0;
+	public static final int FIELD = 1;
+	public static final int EXCEPTION = 2;
 }
