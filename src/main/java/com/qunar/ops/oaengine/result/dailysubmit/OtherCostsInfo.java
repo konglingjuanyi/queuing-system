7dea75f061e7ac5b5b7fabe50d254ffc4d9b6d0d
niannian199 <niannian199@sina.com>
2014-11-20 15:30:23 +0800

    加注释

--- src/main/java/com/qunar/ops/oaengine/result/dailysubmit/OtherCostsInfo.java
+++ src/main/java/com/qunar/ops/oaengine/result/dailysubmit/OtherCostsInfo.java
@@ -4,32 +4,41 @@ import java.io.Serializable;
 
 import com.qunar.ops.oaengine.model.Formson0117;
 
-public class OtherCostsInfo extends Formson0117 implements Serializable{
+public class OtherCostsInfo extends Formson0117 implements Serializable {
 	/**
 	 * 其他费用
 	 */
 	private static final long serialVersionUID = 8933545444370281313L;
-//    private String otherCostComment;
-//    private Long otherCostAmount;
-//    private String otherCostProject;
-    
+
+	// 其他费用备注
+	// private String otherCostComment;
+	// 其他费用金额
+	// private Long otherCostAmount;
+	// 其他费用项目
+	// private String otherCostProject;
+
 	public String getOtherCostComment() {
 		return super.getField0048();
 	}
+
 	public void setOtherCostComment(String otherCostComment) {
 		super.setField0048(otherCostComment);
 	}
+
 	public Long getOtherCostAmount() {
 		return super.getField0049();
 	}
+
 	public void setOtherCostAmount(Long otherCostAmount) {
 		super.setField0049(otherCostAmount);
 	}
+
 	public String getOtherCostProject() {
 		return super.getField0050();
 	}
+
 	public void setOtherCostProject(String otherCostProject) {
 		super.setField0050(otherCostProject);
 	}
-    
+
 }
--- src/main/java/com/qunar/ops/oaengine/result/dailysubmit/OvertimeMealsInfo.java
+++ src/main/java/com/qunar/ops/oaengine/result/dailysubmit/OvertimeMealsInfo.java
@@ -6,72 +6,99 @@ import java.util.Date;
 
 import com.qunar.ops.oaengine.model.Formson0115;
 
-public class OvertimeMealsInfo extends Formson0115 implements Serializable{
+public class OvertimeMealsInfo extends Formson0115 implements Serializable {
 	/**
 	 * 加班餐费
 	 */
 	private static final long serialVersionUID = 8933545444370281313L;
-//	private Date overtimeMealsDate;
-//    private String overtimeMealsComment;
-//    private Long perMealsFee;
-//    private Long overtimeMealsAmount;
-//    private Long mealsPersonNum;
-//    private String overtimeMealsPeerPeople;
-//    private String mealsAddr;
-//    private String invoiceAmount;
-//    private BigDecimal overtimeMealsWorkhours;
-    
+
+	// 加班餐费日期
+	// private Date overtimeMealsDate;
+	// 加班餐费备注
+	// private String overtimeMealsComment;
+	// 人均餐标
+	// private Long perMealsFee;
+	// 加班餐费金额
+	// private Long overtimeMealsAmount;
+	// 就餐人数
+	// private Long mealsPersonNum;
+	// 加班餐费同行人
+	// private String overtimeMealsPeerPeople;
+	// 就餐地点
+	// private String mealsAddr;
+	// 发票金额
+	// private String invoiceAmount;
+	// 加班餐费工时
+	// private BigDecimal overtimeMealsWorkhours;
+
 	public Date getOvertimeMealsDate() {
 		return super.getField0034();
 	}
+
 	public void setOvertimeMealsDate(Date overtimeMealsDate) {
 		super.setField0034(overtimeMealsDate);
 	}
+
 	public String getOvertimeMealsComment() {
 		return super.getField0035();
 	}
+
 	public void setOvertimeMealsComment(String overtimeMealsComment) {
 		super.setField0035(overtimeMealsComment);
 	}
+
 	public Long getPerMealsFee() {
 		return super.getField0036();
 	}
+
 	public void setPerMealsFee(Long perMealsFee) {
 		super.setField0036(perMealsFee);
 	}
+
 	public Long getOvertimeMealsAmount() {
 		return super.getField0037();
 	}
+
 	public void setOvertimeMealsAmount(Long overtimeMealsAmount) {
 		super.setField0037(overtimeMealsAmount);
 	}
+
 	public Long getMealsPersonNum() {
 		return super.getField0038();
 	}
+
 	public void setMealsPersonNum(Long mealsPersonNum) {
 		super.setField0038(mealsPersonNum);
 	}
+
 	public String getOvertimeMealsPeerPeople() {
 		return super.getField0039();
 	}
+
 	public void setOvertimeMealsPeerPeople(String overtimeMealsPeerPeople) {
 		super.setField0039(overtimeMealsPeerPeople);
 	}
+
 	public String getMealsAddr() {
 		return super.getField0040();
 	}
+
 	public void setMealsAddr(String mealsAddr) {
 		super.setField0040(mealsAddr);
 	}
+
 	public String getInvoiceAmount() {
 		return super.getField0077();
 	}
+
 	public void setInvoiceAmount(String invoiceAmount) {
 		super.setField0077(invoiceAmount);
 	}
+
 	public BigDecimal getOvertimeMealsWorkhours() {
 		return super.getField0094();
 	}
+
 	public void setOvertimeMealsWorkhours(BigDecimal overtimeMealsWorkhours) {
 		super.setField0094(overtimeMealsWorkhours);
 	}
