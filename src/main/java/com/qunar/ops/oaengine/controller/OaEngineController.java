710f40bc21feee25f5eabea77384b66c96359320
liulei.liu <liulei.liu@qt-liuleiliu.qunarservers.com>
2015-05-29 15:41:00 +0800

    qss数据补救更新

--- src/main/java/com/qunar/ops/oaengine/controller/OaEngineController.java
+++ src/main/java/com/qunar/ops/oaengine/controller/OaEngineController.java
@@ -97,6 +97,7 @@ import com.qunar.ops.oaengine.result.dailysubmit.TaxiFaresInfo;
 import com.qunar.ops.oaengine.service.EmployeeInfoService;
 import com.qunar.ops.oaengine.service.IOAEngineService;
 import com.qunar.ops.oaengine.service.MailSenderService;
+import com.qunar.ops.oaengine.service.PaymentService;
 import com.qunar.ops.oaengine.util.OAControllerUtils;
 import com.qunar.ops.oaengine.util.OAEngineConst;
 import com.qunar.ops.oaengine.util.QUtils;
@@ -138,6 +139,111 @@ public class OaEngineController {
 
 	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
+	@Autowired
+	protected PaymentService paymentService;
+	
+	@RequestMapping(value = "oa/xxx.html")
+	public ModelAndView xxx(HttpServletRequest request) {
+		paymentService.payment(447);
+		paymentService.payment(738);
+		paymentService.payment(702);
+		paymentService.payment(731);
+		paymentService.payment(717);
+		paymentService.payment(756);
+		paymentService.payment(687);
+		paymentService.payment(678);
+		paymentService.payment(539);
+		paymentService.payment(402);
+		paymentService.payment(610);
+		paymentService.payment(568);
+		paymentService.payment(688);
+		paymentService.payment(672);
+		paymentService.payment(439);
+		paymentService.payment(684);
+		paymentService.payment(496);
+		paymentService.payment(605);
+		paymentService.payment(620);
+		paymentService.payment(646);
+		paymentService.payment(650);
+		paymentService.payment(647);
+		paymentService.payment(618);
+		paymentService.payment(644);
+		paymentService.payment(621);
+		paymentService.payment(765);
+		paymentService.payment(490);
+		paymentService.payment(399);
+		paymentService.payment(635);
+		paymentService.payment(642);
+		paymentService.payment(638);
+		paymentService.payment(639);
+		paymentService.payment(597);
+		paymentService.payment(609);
+		paymentService.payment(622);
+		paymentService.payment(615);
+		paymentService.payment(690);
+		paymentService.payment(733);
+		paymentService.payment(723);
+		paymentService.payment(507);
+		paymentService.payment(523);
+		paymentService.payment(522);
+		paymentService.payment(667);
+		paymentService.payment(708);
+		paymentService.payment(673);
+		paymentService.payment(701);
+		paymentService.payment(634);
+		paymentService.payment(461);
+		paymentService.payment(594);
+		paymentService.payment(543);
+		paymentService.payment(637);
+		paymentService.payment(608);
+		paymentService.payment(364);
+		paymentService.payment(652);
+		paymentService.payment(611);
+		paymentService.payment(659);
+		paymentService.payment(616);
+		paymentService.payment(598);
+		paymentService.payment(773);
+		paymentService.payment(707);
+		paymentService.payment(559);
+		paymentService.payment(664);
+		paymentService.payment(536);
+		paymentService.payment(683);
+		paymentService.payment(695);
+		paymentService.payment(599);
+		paymentService.payment(656);
+		paymentService.payment(715);
+		paymentService.payment(679);
+		paymentService.payment(670);
+		paymentService.payment(606);
+		paymentService.payment(674);
+		paymentService.payment(700);
+		paymentService.payment(535);
+		paymentService.payment(633);
+		paymentService.payment(613);
+		paymentService.payment(692);
+		paymentService.payment(697);
+		paymentService.payment(691);
+		paymentService.payment(630);
+		paymentService.payment(660);
+		paymentService.payment(677);
+		paymentService.payment(722);
+		paymentService.payment(662);
+		paymentService.payment(675);
+		paymentService.payment(727);
+		paymentService.payment(454);
+		paymentService.payment(725);
+		paymentService.payment(710);
+		paymentService.payment(720);
+		paymentService.payment(699);
+		paymentService.payment(693);
+ 
+		String message = "";
+		ModelAndView mav = new ModelAndView("/oa/xxx");
+		mav.addObject("message", message==null?"":message);
+		mav.addObject("debug", OAControllerUtils.isDebug());
+		return mav;
+	}
+	
 	/**
 	 * 修改登陆的用户名
 	 * 
