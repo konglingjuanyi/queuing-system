556eda8ee281363c3416f4e6df1fa9da4bfdb28d
yadong.qin <yadong.qin@qunar.com>
2015-08-19 22:17:04 +0800

    'add'

--- src/main/java/com/qunar/ops/recruit/service/InterviewerService.java
+++ src/main/java/com/qunar/ops/recruit/service/InterviewerService.java
@@ -1,6 +1,7 @@
 package com.qunar.ops.recruit.service;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
