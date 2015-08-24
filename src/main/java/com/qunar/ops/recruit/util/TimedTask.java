f6142ea96f28337a8701a644b5fe81982d85d04c
yadong.qin <yadong.qin@qunar.com>
2015-08-24 15:55:36 +0800

    add

---
+++ src/main/java/com/qunar/ops/recruit/util/TimedTask.java
@@ -0,0 +1,33 @@
+package com.qunar.ops.recruit.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.qunar.ops.recruit.service.WaitService;
+@Component
+public class TimedTask implements Runnable {
+	@Autowired
+	WaitService ws;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		while(true){
+			ws.sort();
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
