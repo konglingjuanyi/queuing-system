5c2f9a8da23f2dbe662ed20984d192c9d764d192
xianhui.zhang <xianhui.zhang@qunar.com>
2015-08-24 21:26:10 +0800

    login

---
+++ src/main/webapp/WEB-INF/static/assets/js/login.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	if($("#msg").html()=='${message}'){
+		$("#msg").text("");
+	}else{
+		$("#msg").css("display","");
+	}
+});
+
+function checkUp(){
+	var username = $("#username").val();
+	var password = $("#password").val();
+	if(username=='' || password==''){
+		$("#msg").text("请输入用户名和密码");
+		$("#msg").css("display","");
+		return false;
+	}
+}
