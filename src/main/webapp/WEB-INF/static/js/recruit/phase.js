457119e94113158eb81f59ae487c01cf4a5adb5f
xianhui.zhang <xianhui.zhang@qunar.com>
2015-08-25 21:13:40 +0800

    1

--- src/main/webapp/WEB-INF/static/js/recruit/phase.js
+++ src/main/webapp/WEB-INF/static/js/recruit/phase.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
 	$('#phase_list').click(function(){
 		$('#content').load('/hr/getPhaseInfos');
 	});
+	
+	
+	$('#addphase').click(function(){
+		$("#addform").modal("show");
+	});
 });
