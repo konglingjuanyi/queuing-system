c05271a4bcfe2ed7ff1074891d222ad1d7e021fa
xianhui.zhang <xianhui.zhang@qunar.com>
2015-08-27 17:42:34 +0800

    modify

--- src/main/java/com/qunar/ops/recruit/service/StudentManageService.java
+++ src/main/java/com/qunar/ops/recruit/service/StudentManageService.java
@@ -1,5 +1,6 @@
 package com.qunar.ops.recruit.service;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
@@ -17,10 +18,19 @@ public class StudentManageService {
 	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	@Autowired
 	StudentMapper stuMapper;
-	public Student createStudent(Row row) {
+	public Student createStudent(Row row) throws Exception {
 		// TODO Auto-generated method stub
 		Student stu=new Student();
 		stu.setNoteNo(getStringCellValue(row.getCell(0)));
+		stu.setYear(getStringCellValue(row.getCell(1)));
+		stu.setPhaseNo(getStringCellValue(row.getCell(2)));
+		String time=getStringCellValue(row.getCell(3));
+		if("".equals(time)){
+			stu.setInterviewTime(null);
+		}else{
+			stu.setInterviewTime(sdf.parse(time));
+		}
+		stu.setName(getStringCellValue(row.getCell(4)));
 		return stu;
 	}
 	
