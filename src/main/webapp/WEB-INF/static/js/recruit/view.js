878b70269d63a8ca21897d89f93a4c4e878ddef3
xianhui.zhang <xianhui.zhang@qunar.com>
2015-08-28 17:49:29 +0800

    modify

--- src/main/webapp/WEB-INF/static/js/recruit/view.js
+++ src/main/webapp/WEB-INF/static/js/recruit/view.js
@@ -60,6 +60,23 @@ $(document).ready(function () {
 	    });
 	});
 	
+	$('#phase_list').click(function(){
+		//window.location.href="/hr/getPhaseInfos";
+		active($('#phasepage'))
+	    $.ajax({
+	      url: "/hr/getPhaseInfos",
+	      type: "POST",
+	      dataType: "html",
+	      contentType: 'application/json; charset=utf-8',
+	      success: function (returnedData) {
+	    	  $('#content').html(returnedData);
+		  },
+	      error: function () {
+	           alert("系统发生了错误请稍后重试");
+	      }
+	    });
+	});
+	
 	$('#index_list').click(function(){
   	    backToIndex();
 	});
