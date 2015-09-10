fcf2c8c44c17e7d3c1d56c0c53cb9fb9d2b3360b
xianhui.zhang <xianhui.zhang@qunar.com>
2015-09-10 21:04:13 +0800

    q

--- src/main/java/com/qunar/ops/recruit/interceptor/AuthInterceptor.java
+++ src/main/java/com/qunar/ops/recruit/interceptor/AuthInterceptor.java
@@ -6,32 +6,36 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.qunar.ops.recruit.util.QUtils;
+import com.qunar.ops.recruit.service.StudentWaiter;
 
 public class AuthInterceptor implements HandlerInterceptor {
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
-//		System.out.println("preHandle============================");
+		//System.out.println("preHandle============================"+request.getRequestURI());
 		if(request.getRequestURI().endsWith("index") || request.getRequestURI().endsWith("login")) {
-			return true;
+			if(!request.getRequestURI().endsWith("toindex")){
+				return true;
+			}else{
+				response.sendRedirect("/error_400.html");
+				return false;
+			}
 		}
 		
-		if(endsWith(request, "index") ||endsWith(request, "login")||
-				endsWith(request, "getAllYears")||endsWith(request, "getPhaseAndCityByYear")||
+		if(endsWith(request, "getAllYears")||endsWith(request, "getPhaseAndCityByYear")||
 				endsWith(request, "updateOprateCity")||endsWith(request, "getCityByYearAndPhase")||
 				endsWith(request, "getAllYears1")||endsWith(request, "getPhaseAndCityByYear1")||
-				endsWith(request, "updateOprateCity1")||endsWith(request, "getCityByYearAndPhase1")){
+				endsWith(request, "updateOprateCity1")||endsWith(request, "getCityByYearAndPhase1")||endsWith(request, "error_400.html")){
 			return true;
 		}
 		
 		
 		Object userId = request.getSession().getAttribute("user");//(String)request.getSession().getAttribute("USER_ID");
-		if(userId != null) 
+		if(userId != null && !(userId instanceof StudentWaiter))
 			return true;
 		else{
-			response.sendError(404);
+			response.sendRedirect("/error_400.html");
 			return false;
 		}
 			
